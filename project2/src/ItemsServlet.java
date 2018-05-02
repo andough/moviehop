7022d5beb6d29d4ab6f2c71e1a3c1111381d3b01
Andrew Wang <xawgunsx@gmail.com>
2018-05-01 20:59:34 -0700

    shopping cart array list to hashmap

--- project2/src/ItemsServlet.java
+++ project2/src/ItemsServlet.java
@@ -6,8 +6,10 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.*;
 
 // Declaring a WebServlet called ItemServlet, which maps to url "/items"
+
 @WebServlet(name = "ItemServlet", urlPatterns = "/items")
 
 public class ItemsServlet extends HttpServlet {
@@ -16,17 +18,18 @@ public class ItemsServlet extends HttpServlet {
                       HttpServletResponse response) throws IOException {
 
         HttpSession session = request.getSession(); // Get a instance of current session on the request
-        ArrayList<String> previousItems = (ArrayList<String>) session.getAttribute("previousItems"); // Retrieve data named "previousItems" from session
+        Map<String,Integer> previousItems = (Map<String,Integer>) session.getAttribute("previousItems"); // Retrieve data named "previousItems" from session
 
         // If "previousItems" is not found on session, means this is a new user, thus we create a new previousItems ArrayList for the user
         if (previousItems == null) {
-            previousItems = new ArrayList<>();
+            previousItems = new HashMap<String,Integer>();
             session.setAttribute("previousItems", previousItems); // Add the newly created ArrayList to session, so that it could be retrieved next time
 
         }
         String newItem = request.getParameter("newItem"); // Get parameter that sent by GET request url
         String clear = request.getParameter("clear");
         String removeItem = request.getParameter("removeItem");
+        String removeAll = request.getParameter("removeAll");
         User username = (User)session.getAttribute("user");
 
         response.setContentType("text/html");
@@ -44,14 +47,26 @@ public class ItemsServlet extends HttpServlet {
         		"	<link rel=\"import\" href=\"bootstrap.html\"></head>\n<body bgcolor=\"#FDF5E6\">\n"
         		+ "<p><div align=\"right\"> account: "+ username.getUsername() +"</div></p><h1>%s <span class=\"glyphicon glyphicon-shopping-cart\"></span></h1>", docType, title, title));
         // In order to prevent multiple clients, requests from altering previousItems ArrayList at the same time, we lock the ArrayList while updating
+        Integer i = 1;
         synchronized (previousItems) {
             if (newItem != null) {
-                previousItems.add(newItem); // Add the new item to the previousItems ArrayList
+				if(previousItems.containsKey(newItem))
+            		i += (Integer)previousItems.get(newItem);
+            	previousItems.put(newItem,i); // Add the new item to the previousItems ArrayList
             }
             
-            if (removeItem != null) {
-                previousItems.remove(removeItem); // remove the new item from the previousItems ArrayList
+            Integer r = 0;
+            if (removeItem != null && previousItems.containsKey(removeItem)) {
+                r = (Integer)previousItems.get(removeItem) -1;
+                if (r > 0)
+                	previousItems.put(removeItem, r);
+                else
+                	previousItems.remove(removeItem);
+                // remove the new item from the previousItems ArrayList
             }
+            
+            if(removeAll != null)
+            	previousItems.remove(removeAll);
 
             // Display the current previousItems ArrayList
             if(clear != null)
@@ -61,13 +76,22 @@ public class ItemsServlet extends HttpServlet {
                 out.println("<i>No items</i>");
             } else {
             	out.println("<table BORDER=\"1\" style=\"width:100%\">");
-            	out.println("<th><center> Title </center></th> <th><center> Cost </center>"
-            			+ "</th> <th> <center> Remove Item </center></th>");
-                for (Object previousItem : previousItems) {
+            	out.println("<th><center> Title </center></th> <th><center> count </center></th>"
+            			+ " <th><center> Cost </center>"
+            			+ "</th> <th> <center> Remove One </center></th>"
+            			+ " <th> <center> Remove All </center></th>");
+            	Iterator it = previousItems.entrySet().iterator();
+            	while (it.hasNext()) {
+                //for (Object previousItem : previousItems) {
+            		Map.Entry pair = (Map.Entry)it.next();
+            		String previousItem = (String)pair.getKey();
+            		Integer count = (Integer)pair.getValue();
                     out.println("<tr><td>" + previousItem + "</td>");
-                    out.println("<td> 10 </td>");
-                    out.println("<td><a href=\"items?removeItem=" + previousItem + 
-                    		"\"><input type=\"submit\" value=\"remove\"></a></td></tr>");
+                    out.println("<td>"+ count +"</td><td> 10 </td>");
+                    out.println("<td><a href=\"items?removeItem=" + previousItem + "\">"
+                    		+ "<input type=\"submit\" value=\"remove\"></a></td>");
+                    out.println("<td><a href=\"items?removeAll=" + previousItem + "\">"
+                    		+ "<input type=\"submit\" value=\"remove All\"></a></td></tr>");
                 }
                 out.println("<a href=\"items?clear=true\">\r\n" + 
                 		"	<p> <button type=\"button\" class=\"btn btn-primary\">clear cart</button> </p>\r\n" + 
