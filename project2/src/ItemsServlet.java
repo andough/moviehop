1a96134e8863c87c19007948a23ffe384e53920d
Andrew Wang <xawgunsx@gmail.com>
2018-05-01 21:39:08 -0700

    connected shopping cart to database

--- project2/src/ItemsServlet.java
+++ project2/src/ItemsServlet.java
@@ -1,18 +1,25 @@
+import javax.annotation.Resource;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.*;
 
 // Declaring a WebServlet called ItemServlet, which maps to url "/items"
-
 @WebServlet(name = "ItemServlet", urlPatterns = "/items")
 
 public class ItemsServlet extends HttpServlet {
+	@Resource(name = "jdbc/moviedb")
+	private DataSource dataSource;
 	
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response) throws IOException {
@@ -47,6 +54,7 @@ public class ItemsServlet extends HttpServlet {
         		"	<link rel=\"import\" href=\"bootstrap.html\"></head>\n<body bgcolor=\"#FDF5E6\">\n"
         		+ "<p><div align=\"right\"> account: "+ username.getUsername() +"</div></p><h1>%s <span class=\"glyphicon glyphicon-shopping-cart\"></span></h1>", docType, title, title));
         // In order to prevent multiple clients, requests from altering previousItems ArrayList at the same time, we lock the ArrayList while updating
+
         Integer i = 1;
         synchronized (previousItems) {
             if (newItem != null) {
@@ -76,27 +84,44 @@ public class ItemsServlet extends HttpServlet {
                 out.println("<i>No items</i>");
             } else {
             	out.println("<table BORDER=\"1\" style=\"width:100%\">");
-            	out.println("<th><center> Title </center></th> <th><center> count </center></th>"
-            			+ " <th><center> Cost </center>"
-            			+ "</th> <th> <center> Remove One </center></th>"
+            	out.println("<th><center> ID </center></th>"
+            			+ "<th><center> Title </center></th> <th><center> count </center></th>"
+            			+ "<th> <center> Remove One </center></th>"
             			+ " <th> <center> Remove All </center></th>");
             	Iterator it = previousItems.entrySet().iterator();
+            	
+            	try {
+                // Create a new connection to database
+                Connection dbCon = dataSource.getConnection();
+
+                // Declare a new statement
+                Statement statement = dbCon.createStatement();
+            		
             	while (it.hasNext()) {
                 //for (Object previousItem : previousItems) {
             		Map.Entry pair = (Map.Entry)it.next();
             		String previousItem = (String)pair.getKey();
             		Integer count = (Integer)pair.getValue();
+            		
+            		String query = String.format("SELECT movies.id, movies.title from movies where movies.id = '%s';", previousItem);
+                    ResultSet rs = statement.executeQuery(query);	
+                    if (rs.next())
+                    {
+            		
                     out.println("<tr><td>" + previousItem + "</td>");
-                    out.println("<td>"+ count +"</td><td> 10 </td>");
+                    out.println("<td>"+ rs.getString("movies.title") +"</td><td>"+ count +"</td>");
                     out.println("<td><a href=\"items?removeItem=" + previousItem + "\">"
                     		+ "<input type=\"submit\" value=\"remove\"></a></td>");
                     out.println("<td><a href=\"items?removeAll=" + previousItem + "\">"
                     		+ "<input type=\"submit\" value=\"remove All\"></a></td></tr>");
+                    }
                 }
                 out.println("<a href=\"items?clear=true\">\r\n" + 
                 		"	<p> <button type=\"button\" class=\"btn btn-primary\">clear cart</button> </p>\r\n" + 
                 		"</a>");
                 out.println("</table>");
+            	}
+            	catch(Exception e) {}
             }
         }
         //out.println("<input type=\"submit\" value=\"remove\">");
